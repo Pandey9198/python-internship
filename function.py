f13283ada0863f8709103f5f00abeb5b03e2a886
Pandey9198 <apandeyji550@gmail.com>
2026-01-20 16:06:58 +0530

    Update function.py
    Refactored the function definitions and added new functions for better clarity. Updated print statements and added functionality to print cricketer roles.

--- function.py
+++ function.py
@@ -7,17 +7,25 @@ abhishek()
 abhishek()
 abhishek() 
 
-# def pandey(name,Address):
-#     # print("hello pandey") 
-#     print(name+Address)
-#     return 9,"bye pandey ji"
-# a=pandey("Abhishek  ",    "noida")
-# print(a)
+def pandey(name,Address):
+    # print("hello pandey") 
+    print(name+Address)
+    return 9,"bye pandey "
+a=pandey("Abhishek  ",    "noida")
+print(a)
 
-# def abhishek( roll,game="Cricket"): #function arguments
-#     print(roll,game)
-# abhishek("allrounder")    
+ #function arguments
+ 
+def abhishek( roll,game="Cricket"):
+    print(roll,game)
+abhishek("allrounder")    
  
     
 
-    
+def MyFuction(cricketer):
+    print(cricketer+ "  allrounder")
+MyFuction("abhishek")
+
+def MyFuctions(cricketer):
+    print(cricketer+  " batsman  ")
+MyFuctions("  rahul")
