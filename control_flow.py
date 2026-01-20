fe4e2d626b4af68669d8b2ac5a430e1097c9f5c2
Pandey9198 <apandeyji550@gmail.com>
2026-01-20 15:11:26 +0530

    Add files via upload

---
+++ control_flow.py
@@ -0,0 +1,26 @@
+# age= int (input("Enter your age"))
+
+# if age>=18:
+    # print("You are eligible to vote")
+    
+# else:
+#     print("you are not aligible to vote")
+
+Marks=int(input("enter the marks:"))
+if Marks >=90:
+    grade="A"
+    
+elif Marks >=80:
+    grade="B"
+    
+elif Marks >=70:
+    grade="c"
+    
+elif Marks>=60:
+    grade="d"
+    
+else:
+    grade="f"
+    
+print(f"Grade: {grade}")
+    
