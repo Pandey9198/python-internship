45d193fb701f3af328c6613dcf01cab51df3d99e
Pandey9198 <apandeyji550@gmail.com>
2026-01-20 15:32:26 +0530

    Update function.py

---
+++ function.py
@@ -0,0 +1,23 @@
+
+def abhishek():
+    print("pandey")
+    print("abhi")
+    # fuction is reuseble
+abhishek()
+abhishek()
+abhishek() 
+
+# def pandey(name,Address):
+#     # print("hello pandey") 
+#     print(name+Address)
+#     return 9,"bye pandey ji"
+# a=pandey("Abhishek  ",    "noida")
+# print(a)
+
+# def abhishek( roll,game="Cricket"): #function arguments
+#     print(roll,game)
+# abhishek("allrounder")    
+ 
+    
+
+    
