07d1586b3e59aee6667cd29c2fb135f00decc8cc
Abhishek <apandeyji550@gmail.com>
2026-01-29 23:07:44 +0530

    update

--- genrator.py
+++ genrator.py
@@ -1,18 +1,25 @@
-def genrator():
-    for i in range(50):
-        yield i
+# def genrator():
+#     for i in range(50):
+#         yield i
         
-push=genrator()
-print(next(push))
-print(next(push))
-print(next(push))
+# push=genrator()
+# print(next(push))
+# print(next(push))
+# print(next(push))
 # for j in push:
 #     print(j)
 
 # def genrator(n):
 #     for i in range(n):
 #         yield i
-        
-print(list(genrator(10)))
-print(tuple(genrator(10)))
-print(set(genrator(10)))
+
+def genrate():
+    for x in range (20):
+        yield x*x
+g=genrate()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
